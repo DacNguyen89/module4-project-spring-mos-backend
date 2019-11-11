c67a89c3b4d697879001d3d37d5af616a1c36595
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-11 23:40:34 +0700

    make user service impl extend UserService

--- src/main/java/com/codegym/mos/module4projectmos/service/impl/UserServiceImplWithSpringData.java
+++ src/main/java/com/codegym/mos/module4projectmos/service/impl/UserServiceImplWithSpringData.java
@@ -1,7 +1,14 @@
 package com.codegym.mos.module4projectmos.service.impl;
 
+import com.codegym.mos.module4projectmos.model.User;
+import com.codegym.mos.module4projectmos.service.UserService;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImplWithSpringData {
+public class UserServiceImplWithSpringData extends UserService {
+    @Override
+    protected CrudRepository<User, Long> repository() {
+        return null;
+    }
 }
