3e7616bc30d14a5e2fef63cc3f3ff155a626643e
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-11 15:10:02 +0700

    Init project

---
+++ src/main/java/com/codegym/mos/module4projectmos/Module4ProjectMosApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.mos.module4projectmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4ProjectMosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4ProjectMosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/mos/module4projectmos/Module4ProjectMosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.mos.module4projectmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4ProjectMosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
