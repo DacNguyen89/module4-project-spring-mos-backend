f82070c30451ff96dd6f062835b7484196da3e6f
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-11 23:39:32 +0700

    Create user service

---
+++ src/main/java/com/codegym/mos/module4projectmos/service/UserService.java
@@ -0,0 +1,6 @@
+package com.codegym.mos.module4projectmos.service;
+
+import com.codegym.mos.module4projectmos.model.User;
+
+public abstract class UserService extends  AbstractEntityServiceImplWithSpringData<User, Long>{
+}
