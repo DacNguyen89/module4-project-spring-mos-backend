272fa0aff26a290d84a4c0caf81995ca96f4becd
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-12 00:45:54 +0700

    inject UserRepository into UserServiceImplWithSpringData

--- src/main/java/com/codegym/mos/module4projectmos/service/impl/UserServiceImplWithSpringData.java
+++ src/main/java/com/codegym/mos/module4projectmos/service/impl/UserServiceImplWithSpringData.java
@@ -1,14 +1,19 @@
 package com.codegym.mos.module4projectmos.service.impl;
 
 import com.codegym.mos.module4projectmos.model.User;
+import com.codegym.mos.module4projectmos.repository.UserRepository;
 import com.codegym.mos.module4projectmos.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImplWithSpringData extends UserService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     protected CrudRepository<User, Long> repository() {
-        return null;
+        return userRepository;
     }
 }
