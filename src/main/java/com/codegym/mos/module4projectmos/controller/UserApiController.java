a8ce4be869941618f4381ac43eef381163f78241
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-11 23:44:17 +0700

    refactor user controller to Api User Controller

--- src/main/java/com/codegym/mos/module4projectmos/controller/UserApiController.java
+++ src/main/java/com/codegym/mos/module4projectmos/controller/UserApiController.java
@@ -3,11 +3,12 @@ package com.codegym.mos.module4projectmos.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/user")
-public class userController {
+public class UserApiController {
     @GetMapping
     public ModelAndView user() {
         ModelAndView modelAndView = new ModelAndView("index");
