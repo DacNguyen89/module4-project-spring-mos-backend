5aa1d2b911603b7aa2943fc8b6968bcf33eb0269
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-14 09:41:28 +0700

    delete abstract entity & add some query

--- src/main/java/com/codegym/mos/module4projectmos/service/UserService.java
+++ src/main/java/com/codegym/mos/module4projectmos/service/UserService.java
@@ -1,6 +1,27 @@
 package com.codegym.mos.module4projectmos.service;
 
-import com.codegym.mos.module4projectmos.model.User;
+import com.codegym.mos.module4projectmos.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public abstract class UserService extends  AbstractEntityServiceImplWithSpringData<User, Long>{
+import java.util.Optional;
+
+public interface UserService {
+    Optional<User> findByUsername(String username);
+
+    Page<User> findAll(Pageable pageable);
+
+    Page<User> findByUsernameContaining(String username, Pageable pageable);
+
+    Page<User> findByRoles_Name(String username, Pageable pageable);
+
+    Optional<User> findById(Long id);
+
+    void save(User user);
+
+    void deleteById(Long id);
+
+    void setFields(User newUserInfo, User oldUserInfo);
+
+    void setFieldsEdit(User oldUserInfo, User newUserInfo);
 }
