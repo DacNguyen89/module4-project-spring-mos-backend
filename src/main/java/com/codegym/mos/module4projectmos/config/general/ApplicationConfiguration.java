bcb563215c5df8f803d78dea8f098bcf5f415dae
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-14 09:10:46 +0700

    create Application config

---
+++ src/main/java/com/codegym/mos/module4projectmos/config/general/ApplicationConfiguration.java
@@ -0,0 +1,34 @@
+package com.codegym.mos.module4projectmos.config.general;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories("com.codegym.mos.module4projectmos.repository")
+@ComponentScan("com.codegym.mos")
+@EnableSpringDataWebSupport
+public class ApplicationConfiguration implements WebMvcConfigurer {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter converter : converters) {
+            if (converter instanceof org.springframework.http.converter.json.MappingJackson2HttpMessageConverter) {
+                ObjectMapper mapper = ((MappingJackson2HttpMessageConverter) converter).getObjectMapper();
+                mapper.registerModule(new Hibernate5Module());
+                // replace Hibernate4Module() with the proper class for your hibernate version.
+            }
+        }
+    }
+}
