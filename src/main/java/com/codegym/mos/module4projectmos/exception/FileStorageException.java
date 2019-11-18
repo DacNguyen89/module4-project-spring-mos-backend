313fa377de93f2ba6ab6f564f773ace70fddcf47
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-18 16:03:56 +0700

    add exceptions handle

---
+++ src/main/java/com/codegym/mos/module4projectmos/exception/FileStorageException.java
@@ -0,0 +1,14 @@
+package com.codegym.mos.module4projectmos.exception;
+
+public class FileStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
