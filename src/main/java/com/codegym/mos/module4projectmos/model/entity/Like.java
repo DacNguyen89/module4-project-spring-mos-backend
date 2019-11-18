d5afd73315d1fe37aadaa928bd4739cc6f115745
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-18 10:14:12 +0700

    add like model

---
+++ src/main/java/com/codegym/mos/module4projectmos/model/entity/Like.java
@@ -0,0 +1,30 @@
+package com.codegym.mos.module4projectmos.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_favorite_songs")
+@Data
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "song_id")
+    private Long songId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    public Like() {
+    }
+
+    ;
+
+    public Like(Long songId, Long userId) {
+        this.songId = songId;
+        this.userId = userId;
+    }
+}
