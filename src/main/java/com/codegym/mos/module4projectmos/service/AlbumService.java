12db3c04c45c132de0dd64b2e23d65827d258167
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-21 09:04:48 +0700

    add album service

---
+++ src/main/java/com/codegym/mos/module4projectmos/service/AlbumService.java
@@ -0,0 +1,13 @@
+package com.codegym.mos.module4projectmos.service;
+
+import com.codegym.mos.module4projectmos.model.entity.Album;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface AlbumService {
+    Page<Album> findAll(Pageable pageable);
+
+    Optional<Album> findById(Long id);
+}
