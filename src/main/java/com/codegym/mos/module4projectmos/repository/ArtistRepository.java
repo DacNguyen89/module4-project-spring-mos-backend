9c84a60d133ef132a7fc794aad5ac1882f062aad
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-21 09:03:48 +0700

    add artist repository

---
+++ src/main/java/com/codegym/mos/module4projectmos/repository/ArtistRepository.java
@@ -0,0 +1,13 @@
+package com.codegym.mos.module4projectmos.repository;
+
+import com.codegym.mos.module4projectmos.model.entity.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+    @Query(value = "SELECT * FROM album WHERE BINARY name=:name", nativeQuery = true)
+    Artist findByName(@Param("name") String name);
+}
