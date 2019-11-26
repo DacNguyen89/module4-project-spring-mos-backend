54a3ecf9c15347c038dd1367af5f8fdb8afc171c
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-26 14:53:28 +0700

    Test deploy Heroku v1.1

--- src/main/java/com/codegym/mos/module4projectmos/service/impl/AvatarStorageService.java
+++ src/main/java/com/codegym/mos/module4projectmos/service/impl/AvatarStorageService.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-//@Service
+@Service
 public class AvatarStorageService extends StorageService<User> {
 /*    final Path avatarStorageLocation;
 
